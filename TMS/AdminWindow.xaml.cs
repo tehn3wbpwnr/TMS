6e3c4befbb75c6109069d53b58beee30b1e6653e
AlexSilveira1 <asilveira8149@conestogac.on.ca>
2022-11-25 16:14:33 -0500

    Made a skeleton of a log file display to the admin

--- TMS/AdminWindow.xaml.cs
+++ TMS/AdminWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,12 +25,31 @@ namespace TMS
         public AdminWindow()
         {
             InitializeComponent();
+            LoadLogFile();
+        }
+
+        private void LoadLogFile ()
+        {
+            List<LogFile> log = new List<LogFile>();
+            // add the files date, time, contents
+            log.Add(new LogFile() { Date = "11/25/2022", Time = "16:08", Contents = "Test3" });
+            log.Add(new LogFile() { Date = "11/25/2022", Time = "16:08", Contents = "Test2" });
+            log.Add(new LogFile() { Date = "11/24/2022", Time = "16:08", Contents = "Test1" });
+
+            logFiles.ItemsSource = log;
         }
 
         protected override void OnClosing(CancelEventArgs e)
         {
             LoginWindow lw = new LoginWindow();
             lw.Show();
+        }
+
+        public class LogFile
+        {
+            public string Date { get; set; }
+            public string Time { get; set; }
+            public string Contents { get; set; }
         }
     }
 }
