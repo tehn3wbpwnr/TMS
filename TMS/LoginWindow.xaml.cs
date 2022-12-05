2d9a0f5ca36dcc99101ade649dffe2881ce79205
AlexSilveira1 <asilveira8149@conestogac.on.ca>
2022-12-05 16:20:18 -0500

    commented login.xaml.cs

--- TMS/LoginWindow.xaml.cs
+++ TMS/LoginWindow.xaml.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * File : LoginWindow.xaml.cs
+ * Assignment : Milestone 4 
+ * Programmers : Alex Silveira, Emanuel Juracic, Josh Moore
+ * First Version: November 10th, 2022
+ * Description : This file contains the code behind for the login window
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
@@ -16,20 +23,34 @@ using System.Windows.Shapes;
 
 namespace TMS
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    //Class     : LoginWindow
+    //Purpose   : The purpose of this class is to only allow the 3 users to login
     public partial class LoginWindow : Window
     {
+        /*
+         * Method       : LoginWindow -- Constructor
+         * Purpose      : This method initializes the window 
+         * Parameters   : none
+         * Returns      : none 
+         */
         public LoginWindow()
         {
             InitializeComponent();
         }
 
+        /*
+         * Method       : btnLoginClick
+         * Purpose      : This method validates the user input for correct login and password
+         * Parameters   : object sender
+         *                RoutedEventArgs e 
+         * Returns      : void 
+         */
         private void btnLogin_Click(object sender, RoutedEventArgs e)
         {
+            //error label instantiated to nothing on click
             lblError.Content = ""; 
 
+            //if admin
             if ((txtUserName.Text == "Admin") && (txtPassword.Password == "AdminPass"))
             {
                 AdminWindow aw = new AdminWindow();
@@ -37,6 +58,7 @@ namespace TMS
                 Close();
             }
 
+            //if buyer
             else if ((txtUserName.Text == "Buyer") && (txtPassword.Password == "BuyerPass"))
             {
                 BuyerWindow bw = new BuyerWindow();
@@ -44,6 +66,7 @@ namespace TMS
                 Close();
             }
 
+            //if planner
             else if ((txtUserName.Text == "Planner") && (txtPassword.Password == "PlannerPass"))
             {
                 PlannerWindow pw = new PlannerWindow();
@@ -51,6 +74,7 @@ namespace TMS
                 Close();
             }
 
+            //invalid 
             else
             {
                 lblError.Content = "Invalid Username or Password";
